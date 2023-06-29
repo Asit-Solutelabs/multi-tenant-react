0401e7eb884a6d7f12d713e2488478e347d24006
Asit-Solutelabs <asit.k@solutelabs.com>
2023-06-29 22:19:16 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import Root from './Components/root';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 function App() {
-  const subdomain = window?.location?.hostname?.split(",")[0]
+  const subdomain = window?.location?.hostname?.split(".")[0]
   const router = createBrowserRouter([
     {
       path: "/",
-      Component : subdomain !== 'localhost' ? Tenant : Root 
+      Component : subdomain !== 'localhost' && subdomain !== process.env.REACT_APP_DOMAIN ? Tenant : Root 
     },
   ]);
  
